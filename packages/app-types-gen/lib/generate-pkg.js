e54c88d21b0522e139f7eb7bd4c56927a5689a46
lvliqi <lvliqi_d@163.com>
2025-07-28 15:57:42 +0800

    feat: up

--- packages/app-types-gen/lib/generate-pkg.js
+++ packages/app-types-gen/lib/generate-pkg.js
@@ -4,6 +4,7 @@ module.exports = ({name, version, groupName='@llqm-types', exposes}, { outDir })
   const packageJson = {
     "name": `${groupName}/${name}`,
     "version": `${version}`,
+    "main": "index.d.ts",
     "scripts": {
       "publish:latest": "npm publish"
     },
