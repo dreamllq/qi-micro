fc1c5850355c90d23e25f56def09cc1da7ffe795
lvliqi <lvliqi_d@163.com>
2025-07-30 11:39:40 +0800

    feat: up

--- packages/webpack-config/main/container.ts
+++ packages/webpack-config/main/container.ts
@@ -6,7 +6,7 @@ import setPublicPath from 'llqm-automation-webpack-plugin/public-path';
   const { setAppsData, initI18n, getLanguage} = await import('llqm-framework-sdk');
   const { load } = await import('llqm-automation-webpack-plugin/locales');
   const language = getLanguage();
-  const messages = (await load[language]())
+  const messages = (await load(language))
   initI18n({messages});
   const appsData = await import('llqm-automation-webpack-plugin/main');
   setAppsData(appsData.default);
