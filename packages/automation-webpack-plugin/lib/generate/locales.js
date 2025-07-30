1e5250a61ca80e566ad542673dfbd53f553ac92f
lvliqi <lvliqi_d@163.com>
2025-07-30 11:27:59 +0800

    feat: up

--- packages/automation-webpack-plugin/lib/generate/locales.js
+++ packages/automation-webpack-plugin/lib/generate/locales.js
@@ -4,7 +4,7 @@ var fs = require('fs');
 var path = require('path');
 exports.default = (function (apps) {
     if (apps === void 0) { apps = []; }
-    var code = "\n    import { mergeJson } from 'llqm-framework-sdk';\n    export const load = async (language:any) => {\n      const locales:any[] = [];\n      \n      ".concat(apps.map(function (app, index) {
+    var code = "\n    import { mergeJson } from 'llqm-framework-sdk';\n    export const load = async (language) => {\n      const locales = [];\n      \n      ".concat(apps.map(function (app, index) {
         return "\n          const supportLanguages".concat(index, " = (await import('").concat(app.name, "/support-languages')).default;\n          const messagesLoader").concat(index, " = (await import('").concat(app.name, "/messages-loader')).default;\n          if (supportLanguages").concat(index, ".includes(language)) {\n            const locale").concat(index, " = await messagesLoader").concat(index, "[language]();\n            locales.push(locale").concat(index, ".default);\n          }\n        ");
     }).join('\n'), "\n      \n\n      return { language: mergeJson(...locales) };\n    };\n  ");
     var to = path.resolve(__dirname, '../../locales.js');
